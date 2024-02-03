47aa20577a6e2f87638337e31f1d683fc0d1ced2
Wil Nichols <hi@wilnichols.design>
2024-02-03 17:30:23 -0500

    adds filters for getting album content from AWS

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 const { DateTime } = require("luxon");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const markdownIt = require('markdown-it');
+const fs = require('fs');
+const { getAverageColor } = require('fast-average-color-node');
+const { S3Client, ListObjectsV2Command } = require("@aws-sdk/client-s3");
+const sizeOf = require('image-size');
 
 module.exports = function(eleventyConfig) {
-  
-  // Misc
   const markdownItOptions = {
       html: true,
       breaks: false,
@@ -27,16 +31,79 @@ module.exports = function(eleventyConfig) {
           }
       })
   })
+  
+  eleventyConfig.setLibrary('md', md);
 
+  // Collections
   eleventyConfig.addCollection("notes", function (collection) {
       return collection.getFilteredByGlob(["notes/**/*.md", "index.md"]);
   });
-
+  
+  // Filters
   eleventyConfig.addFilter("markdownify", string => {
       return md.render(string)
   })
-
-  eleventyConfig.setLibrary('md', md);
+  
+  // simple cache busting method from https://rob.cogit8.org/posts/2020-10-28-simple-11ty-cache-busting/
+  eleventyConfig.addFilter("bust", (url) => {
+    const [urlPart, paramPart] = url.split("?");
+    const params = new URLSearchParams(paramPart || "");
+    params.set("v", DateTime.local().toFormat("X"));
+    return `${urlPart}?${params}`;
+  });
+  
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    dateObj = dateObj.toString();
+    return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  
+  eleventyConfig.addAsyncFilter("getPhotos",  async function(dir) {
+    // do some Async work
+    const client = new S3Client({ 
+      region: "us-east-1" ,
+      credentials: {
+        accessKeyId: process.env.WN_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.WN_AWS_SECRET_ACCESS_KEY
+      }
+    });
+    const albumsParams = {
+      Bucket: 'wnphoto01',
+      Delimiter: '/',
+      Prefix: 'gallery-2023/' + dir + '/'
+    };
+    
+    const command = new ListObjectsV2Command(albumsParams);
+    let data;
+    let albums;
+    try {
+      if (process.env.OFFLINE) {
+        albums = JSON.parse(fs.readFileSync('./_offline/aws/' + dir + '.json'));
+      } else {
+        data = await client.send(command);
+        albums = data.Contents.map(a => a.Key.replace(albumsParams.Prefix, '').replace(albumsParams.Delimiter, ''));
+      };
+      // fs.writeFileSync('./aws-' + dir + '.json', JSON.stringify(albums, null, 1) , 'utf-8');
+    } catch (error) {
+      return 'AWS failure'
+    } finally {
+      return albums;
+    }
+  });
+  
+  eleventyConfig.addAsyncFilter('imageInfo', async function(src) {
+    const path = src.replace('https://wnphoto-1e743.kxcdn.com', '_offline/thumbs').replace('.jpg', '.webp').replace('.png', '.webp');
+    const width = sizeOf(path).width;
+    const height = sizeOf(path).height;
+    async function getColor() {
+      return getAverageColor(path).then(color => {
+          return color.hex;
+      });
+    };
+    const color = await getColor();
+    return {path: path, height: height, width: width, ratio: width/height, color: color};
+  });
+  
+  // Server
   eleventyConfig.setServerOptions({
     liveReload: true
   });
@@ -56,20 +123,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("src/static/css/");
   eleventyConfig.addWatchTarget("src/static/js/");
   eleventyConfig.setWatchThrottleWaitTime(1000); // in milliseconds
-  
-  // Custom Filters
-  // simple cache busting method from https://rob.cogit8.org/posts/2020-10-28-simple-11ty-cache-busting/
-  eleventyConfig.addFilter("bust", (url) => {
-    const [urlPart, paramPart] = url.split("?");
-    const params = new URLSearchParams(paramPart || "");
-    params.set("v", DateTime.local().toFormat("X"));
-    return `${urlPart}?${params}`;
-  });
-  
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    dateObj = dateObj.toString();
-    return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATE_MED);
-  });
   
   return {
     dir: {
