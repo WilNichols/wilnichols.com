6d1a5e472b479c03dbe20c13704d41a30c35b374
Wil Nichols <hi@wilnichols.design>
2024-06-04 11:48:53 -0500

    old meta-desc

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,6 +1,6 @@
 module.exports = {
   lang: 'en',
-  description: 'Working portfolio of multidisciplinary designer Wil Nichols',
+  description: 'Personal website of multidisciplinary designer Wil Nichols',
   contact: {
     email: 'hi@wilnichols.design'
   },
