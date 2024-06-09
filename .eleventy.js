0b6514600b4b89333315e5edfc6e9b1633a92348
Wil Nichols <hi@wilnichols.design>
2024-06-08 23:30:13 -0500

    add video and poster
    pushing now to see if autiplay works when not localhost

--- .eleventy.js
+++ .eleventy.js
@@ -176,6 +176,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"src/robots.txt": "/robots.txt"});
   eleventyConfig.addPassthroughCopy({"src/static/img": "/assets/img"});
   eleventyConfig.addPassthroughCopy({"src/static/js": "/assets/js"});
+  eleventyConfig.addPassthroughCopy({"src/static/vid": "/assets/vid"});
   eleventyConfig.addPassthroughCopy({"src/static/favicon": "/"});
   
   // CSS Mapping
