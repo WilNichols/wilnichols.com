caa7880a2eed15f714871c1f4b3d081735b5841e
Wil Nichols <hi@wilnichols.design>
2024-11-07 15:55:20 -0600

    pen data

--- src/Pens/Pens.11tydata.js
+++ src/Pens/Pens.11tydata.js
@@ -1,5 +1,7 @@
 export default function () {
-  permalink: "/{{ page.fileSlug | slugify }}/index.html",
-  tags: "Pens",
-  layout: "pen.njk"
+  return {
+    permalink: "/{{ page.fileSlug | slugify }}/index.html",
+    tags: "Pens",
+    layout: "pen.njk"
+  }
 }
