2fb392f5fd5e2f03a2e379b3656be68d53bec6c1
Wil Nichols <hi@wilnichols.design>
2024-11-07 15:54:12 -0600

    global data

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,12 +1,14 @@
 export default function () {
-  eleventyComputed: {
-    title: "{{ page.fileSlug }}",
-    month: function (data) {
-      const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-      return month[(new Date(data.date || data.page.date).getMonth())];
-    },
-    year: function (data) {
-      return new Date(data.date || data.page.date).getFullYear();
+  return {
+    eleventyComputed: {
+      title: "{{ page.fileSlug }}",
+      month: function (data) {
+        const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+        return month[(new Date(data.date || data.page.date).getMonth())];
+      },
+      year: function (data) {
+        return new Date(data.date || data.page.date).getFullYear();
+      }
     }
   }
 }
