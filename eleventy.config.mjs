ce01e4b513855a0f123cb05fd11b8b7d08e34007
Wil Nichols <hi@wilnichols.design>
2024-11-07 16:35:03 -0600

    backlinks

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -33,6 +33,16 @@ export default async function(eleventyConfig) {
   .use(markdownItFootnote)
   .use(markdownItTitle)
   .use(function(md) {
+    // Recognize Mediawiki links ([[text]])
+    md.linkify.add("[[", {
+        validate: /^\s?([^\[\]\|\n\r]+)(\|[^\[\]\|\n\r]+)?\s?\]\]/,
+        normalize: match => {
+            const parts = match.raw.slice(2,-2).split("|");
+            parts[0] = parts[0].replace(/.(md|markdown)\s?$/i, "");
+            match.text = (parts[1] || parts[0]).trim();
+            match.url = `/${parts[0].trim().replace(/\s/g, "-").toLowerCase()}/`;
+        }
+    });
       // remove the hr
       md.renderer.rules.footnote_block_open = () => (
         '<section class="footnotes">\n' +
